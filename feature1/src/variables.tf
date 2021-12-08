0d665178b32b61498e5b88cd30e3615ae7fe04d1
prashantha-github <85304071+prashantha-github@users.noreply.github.com>
2021-12-08 18:33:38 +0530

    Create variables.tf

---
+++ feature1/src/variables.tf
@@ -0,0 +1,20 @@
+variable "project_id" {
+  default = "grounded-vista-329411"
+}
+
+variable "network" {
+  default = "network-p"
+}
+
+variable "firewall" {
+  default = "firewall-p"
+}
+
+variable "nat" {
+  default = "nat-p"
+}
+
+variable "router" {
+  default = "router-p"
+}
+© 2021 GitHub, Inc.
